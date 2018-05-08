a33c7541f26f87132dc3ad7d52a0ea9e771e6a6c
Magda_Glodek <magdaglodekk@gmail.com>
2018-05-08 12:27:12 +0200

    zadanie 2-4

---
+++ zad 2-4.R
@@ -0,0 +1,19 @@
+# zad. 2-4
+
+
+
+# zad. 2-3 z pêtle while
+
+
+
+rok <- 1800
+
+while(rok <= 2018) {
+  
+  if (((rok %% 4) == 0) & ((rok %% 100) != 0) | ((rok %% 400) == 0))
+    
+    print(rok)
+  
+  rok <-  rok + 1
+  
+}
