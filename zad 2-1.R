8aae7cff3fe1cd49e6f9c155964ec71092fd613c
Magda_Glodek <magdaglodekk@gmail.com>
2018-05-21 18:11:27 +0200

    uzupełnienie zadania

--- zad 2-1.R
+++ zad 2-1.R
@@ -1,1 +1,63 @@
+# zad. 2-1
 
+
+
+#1
+
+kwartal_1 <- c("Styczen", "Luty", "Marzec")
+
+kwartal_2 <- c("Kwiecien", "Maj", "Czerwiec")
+
+kwartal_3 <- c("Lipiec", "Sierpieñ", "Wrzesien")
+
+kwartal_4 <- c("Pazdziernik", "Listopad", "Grudzien")
+
+
+
+#2
+
+rok <- c(kwartal_1, kwartal_2, kwartal_3, kwartal_4)
+
+
+
+#3
+
+rok[c(1,2)]
+
+
+
+#4
+
+rok[c(8:12)]
+
+
+
+#5
+
+rok[c(1, 3, 5, 7, 11)]
+
+
+
+#6
+
+rok <- c("Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien")
+
+nchar(rok)
+
+
+
+#7
+
+liczba_liter_w_miesiacu <- c(7, 4, 6, 8, 3, 8, 6, 8, 8, 11, 8, 8)
+
+mean(liczba_liter_w_miesiacu)
+
+sd(liczba_liter_w_miesiacu)
+
+
+
+zima <- c(8, 7, 4, 6)
+
+mean(zima)
+
+sd(zima)
